cd82b627802f6def1a7fc43c5baa01086d10747e
riyAdHmobin <riyadhmobin@gmail.com>
2021-04-03 00:00:58 +0600

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="css/style.css">
+
+	</head>
+	<body>
+	
+	    <div class="text w-100">
+		    <h2>Welcome to login</h2>
+			<p>Don't have an account?</p>
+			<a href="#" class="btn btn-white btn-outline-white">Sign Up</a>
+		</div>
+			    
+		<div class="w-100">
+			<h3 class="mb-4">Sign In</h3>
+		</div>
+								
+		<div class="form-group mb-3">
+			<label class="label" for="name">Username</label>
+			<input type="text" class="form-control" placeholder="Username" required>
+		</div>
+
+		<div class="form-group mb-3">
+		    <label class="label" for="password">Password</label>
+		    <input type="password" class="form-control" placeholder="Password" required>
+		 </div>
+
+		<div class="form-group">
+		    <button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
+		 </div>
+	</body>
+</html>
+
